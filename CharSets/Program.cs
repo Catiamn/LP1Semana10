890695a7f0678df79298e291cce4ac62fff297c8
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-05-09 14:36:29 +0100

    Added CharSets

---
+++ CharSets/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace CharSets
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<char> lst = new List<char>();
+            foreach (string arg in args)
+            {
+                try
+                {
+                    string fileContent = File.ReadAllText(arg);
+                    foreach (char c in fileContent)
+                    {
+                        if (!char.IsWhiteSpace(c) && !lst.Contains(c))
+                        {
+                            lst.Add(c);
+                        }
+                    }
+                }
+                catch
+                {
+                    break;
+                }
+            }
+
+            lst.Sort();
+            foreach (char c in lst)
+            {
+                Console.WriteLine(c); 
+            }
+        }
+    }
+}
